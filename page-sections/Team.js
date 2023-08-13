5bf159b9f35fea8fdf9ab01035e79a29dc6ca647
Raymond Yan <raymondyan777@gmail.com>
2023-08-13 13:09:48 -0400

    update bios

--- page-sections/Team.js
+++ page-sections/Team.js
@@ -9,50 +9,64 @@ const TeamSection = () => {
       name: 'Matt',
       title: 'President',
       picture: 'https://robohash.org/Matt.png',
-      bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      bio: 'I\'m a fourth year studying Cyber Security Analytics and Operations. I\'ve been involved in a couple of startups in recent years that spiked my interest in entrepreneurship! I enjoy snowboarding, boxing, and working out outside of academics.',
       linkedin: 'https://www.linkedin.com/in/johndoe'
     },
     {
       name: 'Bianca',
       title: 'Vice President',
       picture: 'https://robohash.org/Bianca.png',
-      bio: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      bio: 'Bianca is a rising junior figuring out her major but enrolled in the innovation and entrepreneurship minor at Penn State. Currently, as the vice president of the entrepreneurship club she loves helping facilitate connections through the entrepreneurship ecosystem. She has ventured into different aspects of startup culture. You can find her working in the prototyping and manufacturing facility OriginLabs. Bianca has served the entrepreneurship community for almost 2 years and will continue to help Penn State entrepreneurship shine!',
       linkedin: 'https://www.linkedin.com/in/janesmith'
     },
     {
       name: 'Ethan',
       title: 'Marketing and Communications Director',
       picture: 'https://robohash.org/YOUR-TEXT.png',
-      bio: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco.',
+      bio: 'Second year aerospace engineering major with a minor in entrepreneurship and innovation. I love football and soccer. Go birds.',
       linkedin: 'https://www.linkedin.com/in/alexjohnson'
     },
     {
         name: 'Pranav',
         title: 'External Relations and Outreach Specialist',
         picture: 'https://robohash.org/YOUR-TEXT.png',
-        bio: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco.',
+        bio: 'I\'m Pranav Mishra – an adventurous soul and committed entrepreneur. As the External Director & Outreach Specialist at the Nittany Entrepreneurship Society, I bring a global perspective shaped by my journey through 12 schools across 15 places. Beyond entrepreneurship, I\'m fueled by a love for adventure sports like hiking and rock climbing, and you\'ll often find me in the boxing ring. My dynamic background enriches our outreach efforts, embodying our mission to foster innovation and entrepreneurial success.',
         linkedin: 'https://www.linkedin.com/in/alexjohnson'
     },
     {
         name: 'Mehul',
         title: 'Events and Ecosystem Coordinator',
         picture: 'https://robohash.org/YOUR-TEXT.png',
-        bio: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco.',
+        bio: '',
         linkedin: 'https://www.linkedin.com/in/alexjohnson'
     },
     {
         name: 'Charlotte',
         title: 'Secretary and Community Connector',
         picture: 'https://robohash.org/YOUR-TEXT.png',
-        bio: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco.',
+        bio: '',
         linkedin: 'https://www.linkedin.com/in/alexjohnson'
     },
     {
         name: 'Heer',
         title: 'Treasurer and Financial Strategist',
         picture: '/alex-johnson.jpg',
-        bio: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco.',
+        bio: '',
         linkedin: 'https://www.linkedin.com/in/alexjohnson'
+    },
+    {
+      name: 'Ray',
+      title: 'Social Chair',
+      picture: '/alex-johnson.jpg',
+      bio: '',
+      linkedin: 'https://www.linkedin.com/in/alexjohnson'
+    },
+    {
+      name: 'Dennis',
+      title: 'Philanthropy Chair',
+      picture: '/alex-johnson.jpg',
+      bio: '',
+      linkedin: 'https://www.linkedin.com/in/alexjohnson'
     },
   ];
 
